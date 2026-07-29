cb4dcd8af5b8451963c4472c7e59a5d934a42392
Sanchit Patel <sanchitpatel1590@gmail.com>
2026-07-29 12:36:16 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        service: resolve(__dirname, 'service.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        cookies: resolve(__dirname, 'cookies.html'),
+        licenses: resolve(__dirname, 'licenses.html'),
+        404: resolve(__dirname, '404.html'),
+      },
+    },
+  },
+});
